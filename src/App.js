7d0ab02c175100c18d23067a043ba6bf7f938656
sarawall1187 <sarawall1187@gmail.com>
2019-09-14 09:00:51 -0700

    setup initial state in App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,41 @@ import React from 'react';
 
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(props){
+    super()
+    this.state = {
+      title: "Nashville Goldsmith",
+      headerLinks: [
+       { title: "Home", path: '/'},
+       { title: "About", path: '/about'},
+       { title: "Contact", path: '/contact'},
+       { title: "Gallery", path: '/gallery'}
+      ],
+      home: {
+        title: 'Nashville\'s Premiere Jewelery',
+        subTitle: 'Specializing in Custom Designs and Antique Jewelry',
+      },
+      about: {
+        title: 'About the Owner'
+      },
+      contact: {
+        title: 'Contact Me'
+      },
+      gallery: {
+        title: 'Gallery'
+      }
+    }
+  }
+ 
+
+  render() {
   return (
     <div className="App">
       <h1>Hello Nashville!</h1>
     </div>
-  );
+  )}
+
 }
 
 export default App;
