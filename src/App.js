b5f7d7aa90d8b47a6c04240e5f95d6ec3051525f
sarawall1187 <sarawall1187@gmail.com>
2019-09-14 09:31:37 -0700

    install react-router-dom, created components folder with files for home, about, contact, gallery

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+import Footer from './components/Footer';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Gallery from './components/Gallery';
 
 class App extends React.Component {
   constructor(props){
@@ -33,35 +41,34 @@ class App extends React.Component {
 
   render() {
   return (
-   
+
       <Router>
       <Container className="p-0" fluid={true}>
-          
+       
           <Navbar className="border-bottom" bg="transparent" expand="lg">
             <Navbar.Brand>Nashville Goldsmith</Navbar.Brand>
-
-            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
-            <Navbar.Collapse id="navbar-toggle">
+       {/* <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" /> */}
+            {/* <Navbar.Collapse id="navbar-toggle">
               <Nav className="ml-auto">
                 <Link className="nav-link" to="/">Home</Link>
                 <Link className="nav-link" to="/about">About the Owner</Link>
                 <Link className="nav-link" to="/contact">Contact Me</Link>
                 <Link className="nav-link" to="/Gallery">Gallery</Link>
               </Nav>
-            </Navbar.Collapse>
+            </Navbar.Collapse> */}
           </Navbar>
 
-          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
-          <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
-          <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
-          <Route path="/gallery" render={() => <ContactPage title={this.state.gallery.title} />} />
+          {/* <Route path="/" exact render={() => <Home title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
+          <Route path="/about" render={() => <About title={this.state.about.title} />} />
+          <Route path="/contact" render={() => <Contact title={this.state.contact.title} />} />
+          <Route path="/gallery" render={() => <Gallery title={this.state.gallery.title} />} />
           
-          <Footer />
+          <Footer /> */}
 
         </Container>
 
       </Router>
-  
+
   )}
 
 }
