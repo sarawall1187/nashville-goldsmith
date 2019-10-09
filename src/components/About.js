92ce0fbea6cc8471879bc8f492bad4eb228a4011
sarawall1187 <sarawall1187@gmail.com>
2019-10-09 11:17:39 -0700

    added image and card to start about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card';
 
 const About = () => {
 
 
 
     return(
-        <h1>About page!</h1>
+         <Card style={{ width: '75%', margin: '0 auto'}}>
+            <Card.Img variant="top" src="mattPic.jpg" bg="black" fluid/>
+            <Card.Body>
+            <Card.Text>
+               Matt is a Nashville Native Been a Jeweler 18 years etc etc.
+            </Card.Text>
+            </Card.Body>
+        </Card>
     )
 }
 
