44b73d63ece1de6ab931511f669aaa181e0608d0
sarawall1187 <sarawall1187@gmail.com>
2019-10-10 09:15:50 -0700

    started gallery page and imported pics

--- src/components/About.js
+++ src/components/About.js
@@ -6,11 +6,14 @@ const About = () => {
 
 
     return(
-         <Card style={{ width: '75%', margin: '0 auto'}}>
-            <Card.Img variant="top" src="mattPic.jpg" bg="black" fluid/>
+         <Card bg="dark" style={{ width: '75%', margin: '0 auto'}}>
+            <Card.Img variant="top" src="mattAboutPic.jpg" fluid/>
             <Card.Body>
-            <Card.Text>
-               Matt is a Nashville Native Been a Jeweler 18 years etc etc.
+            <Card.Text style={{color: 'white'}}>
+               Matt Walker is a Nashville Native who is proud to have been a part of the growth of our beautiful city.
+               He started his career as a jewelery polisher and continued on to complete 3 intense apprenticeships. 
+               He is now a Master Jeweler and Goldsmith who specializes in antique jewelery repair and designing custom pieces.
+               He has a wife and a son, and plans to teach his son the family business when he gets older.  
             </Card.Text>
             </Card.Body>
         </Card>
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,13 +1,93 @@
 import React from 'react'
-
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup'
 
 const Gallery = () => {
 
-
-
     return(
-       <h1>Gallery Page</h1>
+       <>
+        {/* <Card.Group> */}
+       <Card>
+            <Card.Img variant="top" src="goldCasting.jpg" />
+            <Card.Body>
+            <Card.Text>
+                Raw Casting of Gold Ringss
+            </Card.Text>
+            </Card.Body>
+        </Card> 
+
+         {/* <Card>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+            <Card.Text>
+                Some quick example text to build on the card title and make up the bulk
+                of the card's content.
+            </Card.Text>
+            </Card.Body>
+        </Card>
+
+         <Card>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+            <Card.Text>
+                Some quick example text to build on the card title and make up the bulk
+                of the card's content.
+            </Card.Text>
+            </Card.Body>
+        </Card>
+
+         <Card>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+            <Card.Text>
+                Some quick example text to build on the card title and make up the bulk
+                of the card's content.
+            </Card.Text>
+            </Card.Body>
+        </Card>
+
+         <Card>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+            <Card.Text>
+                Some quick example text to build on the card title and make up the bulk
+                of the card's content.
+            </Card.Text>
+            </Card.Body>
+        </Card>
+
+         <Card>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+            <Card.Text>
+                Some quick example text to build on the card title and make up the bulk
+                of the card's content.
+            </Card.Text>
+            </Card.Body>
+        </Card>
+
+         <Card>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+            <Card.Text>
+                Some quick example text to build on the card title and make up the bulk
+                of the card's content.
+            </Card.Text>
+            </Card.Body>
+        </Card>
+
+         <Card>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+            <Card.Text>
+                Some quick example text to build on the card title and make up the bulk
+                of the card's content.
+            </Card.Text>
+            </Card.Body>
+        </Card> */}
+        {/* </Card.Group> */}
+       </>
     )
 }
 
-export default Gallery
+export default Gallery;
