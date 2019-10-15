0e35b9a30ec325e0b4facece5e365c8264f74c22
sarawall1187 <sarawall1187@gmail.com>
2019-10-15 12:21:32 -0700

    code cleanup

--- src/components/About.js
+++ src/components/About.js
@@ -7,14 +7,14 @@ const About = () => {
 
     return(
          <Card bg="dark" style={{ width: '75%', margin: '0 auto'}}>
-            <Card.Img variant="top" src="mattAboutPic.jpg" fluid/>
+          <Card.Img variant="top" src="mattAboutPic.jpg" fluid/>
             <Card.Body>
-            <Card.Text style={{color: 'white'}}>
-               Matt Walker is a Nashville Native who is proud to have been a part of the growth of our beautiful city.
-               He started his career as a jewelery polisher and continued on to complete 3 intense apprenticeships. 
-               He is now a Master Jeweler and Goldsmith who specializes in antique jewelery repair and designing custom pieces.
-               He has a wife and a son, and plans to teach his son the family business when he gets older.  
-            </Card.Text>
+                <Card.Text style={{color: 'white'}}>
+                Matt Walker is a Nashville Native who is proud to have been a part of the growth of our beautiful city.
+                He started his career as a jewelery polisher and continued on to complete 3 intense apprenticeships. 
+                He is now a Master Jeweler and Goldsmith who specializes in antique jewelery repair and designing custom pieces.
+                He has a wife and a son, and plans to teach his son the family business when he gets older.  
+                </Card.Text>
             </Card.Body>
         </Card>
     )
--- src/components/Home.js
+++ src/components/Home.js
@@ -8,8 +8,7 @@ const Home = (props) => {
 
     return(
         <> 
-         <Hero title={props.title} subTitle={props.subTitle}/>
-        
+         <Hero title={props.title} subTitle={props.subTitle}/>  
          <CarouselPage/>   
         </>
     )
