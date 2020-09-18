9a9a8a8fc8bec2f9663d20375c38f29a37ae661e
sarawall1187 <sara.walker@lifeway.com>
2020-09-18 14:49:26 -0500

    add form components to contact page

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,73 +1,76 @@
-import React from 'react'
+import React from "react";
 
-import Card from 'react-bootstrap/Card';
-import Carousel from 'react-bootstrap/Carousel'
-
+import Card from "react-bootstrap/Card";
+import Carousel from "react-bootstrap/Carousel";
 
 class CarouselPage extends React.Component {
+  render() {
+    return (
+      <>
+        <Carousel className="carouselWidth">
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="/jewelerworking.jpg"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>Call Us!</h3>
+              <h4>
+                <a className="yellow" href="tel:615-426-1733">
+                  615-426-1733
+                </a>
+              </h4>
+              <p>We give quotes or an appointment for faster service.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
 
-    render() {
-        return(
-            <>
-            <Carousel className="carouselWidth">
-                <Carousel.Item >
-                    <img
-                    className="d-block w-100"
-                    src="/jewelerworking.jpg"
-                    alt="First slide"
-                    />
-                    <Carousel.Caption>
-                    <h3>Call Us!</h3>
-                    <h4><a className="yellow" href="tel:615-426-1733">615-426-1733</a></h4>
-                    <p>We give quotes or an appointment for faster service.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
+          <Carousel.Item className="servicesCard">
+            <img
+              className="d-block w-100"
+              src="jeweler.jpg"
+              alt="Third slide"
+            />
 
-                <Carousel.Item  className="servicesCard">
-                    <img
-                    className="d-block w-100"
-                    src="jeweler.jpg"
-                    alt="Third slide"
-                    />
+            <Carousel.Caption className="servicesList">
+              <Card bg="transparent">
+                <Card.Header as="h4" className="yellow">
+                  Services Provided:
+                </Card.Header>
+                <Card.Body className="black">
+                  <ul>
+                    <li>Custom Jewelry Design</li>
+                    <li>3D Printing</li>
+                    <li>Diamond Replacement</li>
+                    <li>Diamond Setting</li>
+                    <li>Laser Repair</li>
+                    <li>Ring Sizing</li>
+                    <li>Ring Refinishing</li>
+                    <li>Antique Jewelry Repair</li>
+                    <li>Watch Repair including Rolex</li>
+                    <li>Buy Gold and Diamonds</li>
+                    <li>Plus much more!</li>
+                  </ul>
+                </Card.Body>
+              </Card>
+            </Carousel.Caption>
+          </Carousel.Item>
 
-                    <Carousel.Caption className="servicesList">
-                    <Card bg="transparent">
-                    <Card.Header as="h4" className="yellow">Services Provided:</Card.Header>
-                    <Card.Body className="black" >
-                        <ul>
-                            <li>Diamond Replacement</li>
-                            <li>Diamond Setting</li>
-                            <li>Laser Repair</li>
-                            <li>Solder Repair</li>
-                            <li>Ring Sizing</li>
-                            <li>Ring Refinishing</li>
-                            <li>Antique Jewelry Repair</li>
-                            <li>Custom Jewelry Design</li>
-                            <li>Watch Repair including Rolex</li>
-                            <li>Buy Gold and Diamonds</li>
-                            <li>Plus much more!</li>
-                        </ul>
-                    </Card.Body>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="bandspic.jpg"
+              alt="Third slide"
+            />
 
-                    </Card>
-                    </Carousel.Caption>
-                </Carousel.Item>
-
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src="bandspic.jpg"
-                    alt="Third slide"
-                    />
-
-                    <Carousel.Caption>
-                    <h3 className="yellow">Make Her Dreams Come True!</h3>
-                    <p className="yellow">We can make any piece you can dream up!</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
-        </>
-        )
-    }
+            <Carousel.Caption>
+              <h3 className="yellow">Make Her Dreams Come True!</h3>
+              <p className="yellow">We can make any piece you can dream of!</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </>
+    );
+  }
 }
-export default CarouselPage
+export default CarouselPage;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,93 @@
-import React from 'react'
+import React from "react";
+import Hero from "../components/Hero";
+import Form from "react-bootstrap/form";
+import { Button } from "react-bootstrap";
 
-const Contact = () => {
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      message: "",
+      disabled: false,
+      emailSent: null,
+    };
+  }
 
+  handleChange = (event) => {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
 
+    this.setState({
+      [name]: value,
+    });
+  };
 
-    return(
-        <h1>Contact page!</h1>
-    )
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.setState({
+      disabled: true,
+      emailSent: true,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Hero title={this.props.title} />
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group>
+            <Form.Label htmlFor="full-name">Full Name</Form.Label>
+            <Form.Control
+              id="full-name"
+              name="name"
+              type="text"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label htmlFor="email">Email</Form.Label>
+            <Form.Control
+              id="email"
+              name="email"
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label htmlFor="message">Message</Form.Label>
+            <Form.Control
+              id="message"
+              name="message"
+              as="textarea"
+              rows="5"
+              value={this.state.message}
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+          <Button
+            className="d-inline-block"
+            variant="primary"
+            type="submit"
+            disabled={this.state.disabled}
+          >
+            Send
+          </Button>
+          {this.state.emailSent === true && (
+            <p className="d-inline success-msg">Email Sent</p>
+          )}
+          {this.state.emailSent === false && (
+            <p className="d-inline err-msg">Error - Email Not Sent</p>
+          )}
+        </Form>
+      </div>
+    );
+  }
 }
 
-export default Contact
+export default Contact;
